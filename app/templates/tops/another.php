09a96085a06b9b7e40cffc631749962d714bd83f
hideri <hori.hide1003@gmail.com>
2021-05-23 10:15:22 +0900

    split chunks plugin追加

--- app/templates/tops/another.php
+++ app/templates/tops/another.php
@@ -1,2 +1,3 @@
 <h1>Index</h1>
 <?php echo ($this->Html->script(['public/another.bundle'])); ?>
+<?php echo ($this->Html->script(['public/vendor'])); ?>
--- app/webpack.common.js
+++ app/webpack.common.js
@@ -9,5 +9,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "webroot/js/public"),
         filename: "[name].bundle.js",
+        chunkFilename: "[name].js", // チャンクで分割するファイル名 entrypoint以外からのファイル名
+    },
+    // splichunckpluginの設定 どのファイルを分割するか
+    optimization: {
+        splitChunks: {
+            chunks: "initial", // 静的にimportしているモジュールが分割対象になる デフォルトだとnode_modules以下
+            name: "vendor",
+        },
     },
 };
